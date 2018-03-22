926513a64180acc1ad7ab9f80f90f07b05ff2f68
Lobachev27 <Lobachevd27@gmail.com>
2018-03-22 22:00:05 +0300

    Header was added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from "react";
 
+import "./App.css";
 import Header from "./Header";
-import Directions from "./Directions";
-import Prices from "./Prices";
-import Partners from "./Partners";
-import Subscribe from "./Subscribe";
-import Offer from "./Offer";
-import Help from "./Help";
-import MobileApp from "./MobileApp";
-import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
-		<div>
-		  <Header />
-		  <Directions />
-		  <Prices />
-		  <Partners />
-		  <Subscribe />
-		  <Offer />
-		  <Help />
-		  <MobileApp />
-		  <Footer />
-		</div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
---
+++ src/Header/index.js
@@ -0,0 +1,313 @@
+import React from "react";
+import styled from "styled-components";
+
+import logo from "./logo.svg";
+import aero from "./aero.svg";
+import arrows from "./arrows.svg";
+import calendar from "./calendar.svg";
+import popdown from "./popdn.svg";
+
+const Header = styled.header`
+  display: flex;
+  min-width: 320px;
+  padding-bottom: 88px;
+  background: linear-gradient(
+    148.48deg,
+    #00b0de 22.46%,
+    #01aedc 26.15%,
+    #02abdb 29.89%,
+    #02abdb 33.59%,
+    #02abdb 33.81%,
+    #196ebd 122.26%
+  );
+
+  @media (min-width: 768px) {
+    padding-bottom: 122px;
+  }
+
+  @media (min-width: 1024px) {
+    padding-bottom: 254px;
+  }
+`;
+
+const Logo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 12px;
+  justify-content: flex-start;
+`;
+
+const Image = styled.img`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Text = styled.span`
+  display: none;
+  line-height: 25px;
+  font-weight: normal;
+  font-size: 20px;
+  color: #ffffff;
+  margin: 0 0 0 12px;
+
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
+
+const Title = styled.h1`
+  color: #fff;
+  font-size: 20px;
+  line-height: 23px;
+  font-weight: 700;
+  text-align: center;
+  margin-top: 47px;
+  margin-bottom: 0px;
+
+  @media (min-width: 768px) {
+    line-height: 48px;
+    font-size: 32px;
+    margin-top: 83px;
+  }
+
+  @media (min-width: 1024px) {
+    line-height: 48px;
+    font-size: 40px;
+    margin-top: 215px;
+  }
+`;
+
+const SubTitle = styled.h2`
+  display: none;
+  font-weight: bold;
+  line-height: normal;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 8px;
+
+  @media (min-width: 768px) {
+    display: block;
+    font-size: 24px;
+    line-height: 28px;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 24px;
+    line-height: 28px;
+  }
+`;
+
+const Form = styled.form`
+  margin-top: 16px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  @media (min-width: 768px) {
+    margin-top: 40px;
+  }
+`;
+
+const InputBig = styled.div`
+  flex-basis: 100%;
+
+  @media (min-width: 768px) {
+    flex-basis: 50%;
+  }
+
+  @media (min-width: 1024px) {
+    flex-basis: 23%;
+  }
+`;
+
+const InputSmall = styled.div`
+  flex-basis: 50%;
+
+  @media (min-width: 768px) {
+    flex-basis: 25%;
+  }
+
+  @media (min-width: 1024px) {
+    flex-basis: 15.5%;
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 2px;
+  color: #4a4a4a;
+  background: #ffffff;
+  position: relative;
+  margin: 1px;
+`;
+
+const PlaceInput = styled.input`
+  color: inherit;
+  padding: 18px 0 18px 16px;
+  width: 100%;
+  flex-grow: 1;
+  box-sizing: border-box;
+  border: none;
+  border-radius: inherit;
+  ::placeholder {
+    color: #a0b0b9;
+  }
+
+  @media (min-width: 1024px) {
+    padding: 18px 0 18px 10px;
+  }
+`;
+
+const SwapPlaces = styled.button`
+  margin-right: 16px;
+  margin-left: 10px;
+  border: none;
+  width: 30px;
+  height: 20px;
+  padding: 0;
+  background: url(${arrows}) center no-repeat;
+
+  @media (min-width: 1024px) {
+    margin-right: 15px;
+    margin-left: 10px;
+  }
+`;
+
+const IATACode = styled.span`
+  color: #a0b0b9;
+`;
+
+const Calendar = styled.button`
+  margin-right: 16px;
+  margin-left: 10px;
+  border: none;
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  background: url(${calendar}) center no-repeat;
+
+  @media (min-width: 1024px) {
+    margin-right: 15px;
+    margin-left: 10px;
+  }
+`;
+
+const PaxButton = styled.button`
+  background: #ffffff;
+  color: inherit;
+  border-radius: inherit;
+  border: none;
+  padding: 18px 0 18px 16px;
+  width: 100%;
+  box-sizing: border-box;
+  text-align: left;
+  position: relative;
+
+  @media (min-width: 1024px) {
+    padding: 18px 0 18px 10px;
+  }
+`;
+
+const PaxAdditional = styled.span`
+  color: #a0b0b9;
+`;
+
+const PopDownMark = styled.img`
+  position: absolute;
+  right: 16px;
+  top: 0px;
+  padding: 28px 0 27px 10px;
+  background: url(${popdown}) center no-repeat;
+
+  @media (min-width: 1024px) {
+    right: 12px;
+  }
+`;
+
+const SubmitButton = styled.button`
+  border-radius: 4px;
+  background: #ff9241;
+  border: none;
+  font-size: 24px;
+  font-weight: 700;
+  color: #ffffff;
+  margin-top: 16px;
+  padding: 14px;
+  width: 100%;
+  box-sizing: border-box;
+
+  @media (min-width: 768px) {
+    margin-top: 32px;
+    padding: 15px;
+    font-size: 28px;
+  }
+
+  @media (min-width: 1024px) {
+    margin-top: 48px;
+    padding: 16px;
+  }
+`;
+
+export default () => (
+  <Header>
+    <div className="container">
+      <Logo>
+        <Image src={logo} alt="logo" />
+        <Text> aviasales</Text>
+      </Logo>
+      <Title>Поиск дешевых авиабилетов</Title>
+      <SubTitle>Лучший способ купить авиабилеты дешево</SubTitle>
+      <div className="row">
+        <div className="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10">
+          <Form>
+            <InputBig>
+              <InputWrapper>
+                <PlaceInput defaultValue="Москва" placeholder="Город вылета" />
+                <IATACode>MOW</IATACode>
+                <SwapPlaces />
+              </InputWrapper>
+            </InputBig>
+            <InputBig>
+              <InputWrapper>
+                <PlaceInput value="" placeholder="Город прибытия" />
+                <IATACode />
+              </InputWrapper>
+            </InputBig>
+            <InputSmall>
+              <InputWrapper>
+                <PlaceInput value="" placeholder="Туда" />
+                <Calendar />
+              </InputWrapper>
+            </InputSmall>
+            <InputSmall>
+              <InputWrapper>
+                <PlaceInput value="" placeholder="Обратно" />
+                <Calendar />
+              </InputWrapper>
+            </InputSmall>
+            <InputBig>
+              <InputWrapper>
+                <PaxButton>
+                  1 пассажир, <PaxAdditional>эконом</PaxAdditional>
+                  <PopDownMark />
+                </PaxButton>
+              </InputWrapper>
+            </InputBig>
+          </Form>
+        </div>
+      </div>
+      <div className="row center-xs">
+        <div className="col-xs-12 col-md-6 col-lg-4">
+          <SubmitButton>
+            Найти билеты <img src={aero} alt="aero" />
+          </SubmitButton>
+        </div>
+      </div>
+    </div>
+  </Header>
+);
